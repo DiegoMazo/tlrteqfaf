e410621403bd95e798052aee53fe8a8d47dc3be9
Cromis <diegomazoz321@gmail.com>
2025-06-06 17:02:41 -0500

    fix: simple panel system WIP

---
+++ Assets/Scripts/CromisDev/SimplePanelManager/Panel.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+namespace CromisDev.SimplePanelSystem
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public class Panel : MonoBehaviour
+    {
+        [Header("Transition Settings")]
+        [SerializeField] private float fadeDuration = 0.3f;
+        [SerializeField] private bool startHidden = true;
+
+        private CanvasGroup canvasGroup;
+
+        private void SetActive(bool value)
+        {
+            gameObject.SetActive(value);
+        }
+
+        protected virtual void Awake()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+
+            canvasGroup.interactable = false;
+            canvasGroup.blocksRaycasts = false;
+
+            if (startHidden)
+            {
+                Hide();
+            }
+            else
+            {
+                Show();
+            }
+        }
+
+        public void Show()
+        {
+            SetActive(true);
+            canvasGroup.alpha = 1f;
+            canvasGroup.interactable = true;
+            canvasGroup.blocksRaycasts = true;
+        }
+
+        public void Hide()
+        {
+            canvasGroup.alpha = 0f;
+            canvasGroup.interactable = false;
+            canvasGroup.blocksRaycasts = false;
+            SetActive(false);
+        }
+    }
+}
